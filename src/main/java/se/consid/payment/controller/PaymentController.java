0470072c159c1f63e0079e8544e1e1257aec8b9b
rasmus.tibell <rasmus.tibell@gmail.com>
2024-04-17 17:50:24 +0200

    Fixed class with wrong name.

--- src/main/java/se/consid/payment/controller/PaymentController.java
+++ src/main/java/se/consid/payment/controller/PaymentController.java
@@ -3,7 +3,6 @@ package se.consid.payment.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
